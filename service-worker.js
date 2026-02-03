bf79a46a2040e11bd489264587b7a820e2530a74
M Ibrar khan <ibrarkhan1k566@gmail.com>
2026-02-03 21:14:28 +0500

    pwa update

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,35 @@
+const CACHE_NAME = 'bmi-app-v1';
+const ASSETS_TO_CACHE = [
+  '/',
+  '/index.html',
+  '/StyleSheet.css',
+  '/Script.js',
+  '/manifest.json',
+  '/icon1.png',
+  '/icon2.png'
+];
+
+// Install Event - Cache Files
 self.addEventListener('install', event => {
-  self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Opened cache');
+        return cache.addAll(ASSETS_TO_CACHE);
+      })
+  );
 });
 
+// Fetch Event - Serve from Cache if offline
 self.addEventListener('fetch', event => {
-  event.respondWith(fetch(event.request));
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
 });
