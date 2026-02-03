adbd0868f3043af1bc086fcb63ec4faa0e801822
M Ibrar khan <ibrarkhan1k566@gmail.com>
2026-02-03 19:00:55 +0500

    PWA ADD

--- Script.js
+++ Script.js
@@ -197,10 +197,3 @@ function restartCalculator() {
     
     showStep(0);
 }
-self.addEventListener('install', event => {
-  self.skipWaiting();
-});
-
-self.addEventListener('fetch', event => {
-  event.respondWith(fetch(event.request));
-});
